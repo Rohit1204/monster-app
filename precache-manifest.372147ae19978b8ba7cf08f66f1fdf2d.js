0736f566773dba18f0cfa67603575db280ae08d1
Rohit1204 <rohitahuja725@gmail.com>
2020-05-08 13:39:00 +0530

    Updates

---
+++ precache-manifest.372147ae19978b8ba7cf08f66f1fdf2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c522796fcda5fd213955129f30b79df",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "e3172da856a7396b75d7",
+    "url": "/monster-app/static/css/main.8c10478c.chunk.css"
+  },
+  {
+    "revision": "cecf738d584200afd7be",
+    "url": "/monster-app/static/js/2.c8a84cf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.c8a84cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3172da856a7396b75d7",
+    "url": "/monster-app/static/js/main.a5a40a3f.chunk.js"
+  },
+  {
+    "revision": "af43191e3cbd06fac530",
+    "url": "/monster-app/static/js/runtime-main.6e418424.js"
+  }
+]);
